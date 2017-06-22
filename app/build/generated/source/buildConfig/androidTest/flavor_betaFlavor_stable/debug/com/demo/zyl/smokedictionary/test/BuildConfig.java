3f8f8c9b032621f22e4a347be9ce43f76e5ef372
赵永亮 <zhaoyongliang@zhaoyongliangdeMacBook-Pro.local>
2017-06-22 15:33:10 +0800

    update output apk name

---
+++ app/build/generated/source/buildConfig/androidTest/flavor_betaFlavor_stable/debug/com/demo/zyl/smokedictionary/test/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.demo.zyl.smokedictionary.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.demo.zyl.smokedictionary.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "flavor_betaFlavor_stable";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "香烟管家_beta_1.1build(2)";
+  public static final String FLAVOR_beta = "flavor_beta";
+  public static final String FLAVOR_stable = "flavor_stable";
+}
---
+++ app/build/generated/source/buildConfig/devMain/debug/com/demo/zyl/smokedictionary/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.demo.zyl.smokedictionary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.demo.zyl.smokedictionary";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "devMain";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1";
+  public static final String FLAVOR_beta = "dev";
+  public static final String FLAVOR_stable = "main";
+}
